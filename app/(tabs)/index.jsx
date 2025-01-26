9736c2556b3a477a7be96ee21eb9adf0143cab36
Roman <108236042+romanbaba@users.noreply.github.com>
2025-01-26 14:17:46 +0300

    ✅ Added FAB

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,17 +1,27 @@
-import { router } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons'; // İkonlar için
 import { StatusBar } from 'expo-status-bar';
 import { Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View className="h-full flex justify-center">
-        <Text className="text-4xl font-semibold text-center">Başlangıç Sayfası</Text>
-        <Pressable onPress={() => router.push("/sa")}>
-          <Text className="text-center mt-2 underline">Geri dön</Text>
-        </Pressable>
+    <SafeAreaView className="bg-[#0a090e] flex-1">
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-[#fafafa]">ekli anime listeniz yok eklemek için artıya bas</Text>
       </View>
+
+      <Pressable
+        className="absolute bottom-6 right-6 bg-[#111113] w-[60px] h-[60px] rounded-full flex justify-center items-center shadow-md shadow-black/20"
+        onPress={() => {
+          alert('FAB clicked!');
+        }}
+      >
+        <Ionicons name="add" size={30} color="#fff" />
+      </Pressable>
+
       <StatusBar />
     </SafeAreaView>
-  )
+  );
 }
